c92f3147df99c32d16946fd5f7afde3a3b14e030
Danny <gh@hdcola.org>
2025-04-28 21:04:56 -0400

    add tools list to env

--- income_tax_agent/agent.py
+++ income_tax_agent/agent.py
@@ -1,10 +1,40 @@
 from google.adk.agents import Agent
+import os
 
 from income_tax_agent.ufile.ufile_t3 import get_all_t3, get_t3_info, add_t3, remove_t3
 from income_tax_agent.ufile.ufile_t3_update import update_t3
 from income_tax_agent.ufile.ufile_t5 import get_all_t5, get_t5_info, add_t5, remove_t5, update_t5
 from income_tax_agent.ufile.ufile_person import get_all_person_names, remove_person, add_spouse
 from income_tax_agent.ufile.ufile_t4 import get_all_t4, add_t4, remove_t4, get_t4_info, update_t4_info
+
+tools = []
+
+# Load environment variables
+TOOLS_LIST = os.getenv("AGENT_TOOLS_LIST", "t3,t4,t5,person").split(",")
+
+t4_tools = [
+    get_all_t4, add_t4, remove_t4, get_t4_info, update_t4_info
+]
+if "t4" in TOOLS_LIST:
+    tools.extend(t4_tools)
+
+t5_tools = [
+    get_all_t5, get_t5_info, add_t5, remove_t5, update_t5
+]
+if "t5" in TOOLS_LIST:
+    tools.extend(t5_tools)
+
+t3_tools = [
+    get_all_t3, get_t3_info, add_t3, remove_t3, update_t3
+]
+if "t3" in TOOLS_LIST:
+    tools.extend(t3_tools)
+
+person_tools = [
+    get_all_person_names, remove_person, add_spouse
+]
+if "person" in TOOLS_LIST:
+    tools.extend(person_tools)
 
 root_agent = Agent(
     name="IncomeTaxAgent",
@@ -14,6 +44,5 @@ root_agent = Agent(
         "You are a tax agent in Canada. You can help users fill out their tax returns. "
         "You can answer questions about tax returns, provide information about tax laws, and assist with the filing process. "
     ),
-    tools=[get_all_t5, get_t5_info, get_all_t3, get_t3_info,
-           update_t3, add_t3, remove_t3, add_t5, remove_t5, update_t5, get_all_person_names, remove_person, add_spouse, get_all_t4, add_t4, remove_t4, get_t4_info, update_t4_info],
+    tools=tools,
 )
