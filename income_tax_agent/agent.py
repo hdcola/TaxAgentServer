d37d9df53c230ec6d855e66f11c8b651464431d6
Danny <gh@hdcola.org>
2025-04-29 20:51:03 -0400

    use get_all_slips replace get all t4 t5 t3

--- income_tax_agent/agent.py
+++ income_tax_agent/agent.py
@@ -2,31 +2,31 @@ from google.adk.agents import Agent, LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 import os
 
-from income_tax_agent.ufile.ufile_t3 import get_all_t3, get_t3_info, add_t3, remove_t3
+from income_tax_agent.ufile.ufile_t3 import get_t3_info, add_t3, remove_t3
 from income_tax_agent.ufile.ufile_t3_update import update_t3
-from income_tax_agent.ufile.ufile_t5 import get_all_t5, get_t5_info, add_t5, remove_t5, update_t5
+from income_tax_agent.ufile.ufile_t5 import get_t5_info, add_t5, remove_t5, update_t5
 from income_tax_agent.ufile.ufile_person import get_all_person_names, remove_person, add_spouse
-from income_tax_agent.ufile.ufile_t4 import get_all_t4, add_t4, remove_t4, get_t4_info, update_t4_info
-
-tools = []
+from income_tax_agent.ufile.ufile_t4 import add_t4, remove_t4, get_t4_info, update_t4_info
+from income_tax_agent.ufile.ufile_util import get_all_slips
+tools = [get_all_slips]
 
 # Load environment variables
 TOOLS_LIST = os.getenv("AGENT_TOOLS_LIST", "t3,t4,t5,person").split(",")
 
 t4_tools = [
-    get_all_t4, add_t4, remove_t4, get_t4_info, update_t4_info
+    add_t4, remove_t4, get_t4_info, update_t4_info
 ]
 if "t4" in TOOLS_LIST:
     tools.extend(t4_tools)
 
 t5_tools = [
-    get_all_t5, get_t5_info, add_t5, remove_t5, update_t5
+    get_t5_info, add_t5, remove_t5, update_t5
 ]
 if "t5" in TOOLS_LIST:
     tools.extend(t5_tools)
 
 t3_tools = [
-    get_all_t3, get_t3_info, add_t3, remove_t3, update_t3
+    get_t3_info, add_t3, remove_t3, update_t3
 ]
 if "t3" in TOOLS_LIST:
     tools.extend(t3_tools)
